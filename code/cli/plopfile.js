e4b6256020742b7810e46db4cf72e62ab4b29a76
国中 <zhangguozhong@kaike.la>
2020-07-05 23:21:24 +0800

    feat: 补全cli代码

--- code/cli/plopfile.js
+++ code/cli/plopfile.js
@@ -2,32 +2,31 @@ module.exports = function (plop) {
     const glob = require('glob');
     const path = require('path');
     const files = glob.sync('template/**/*.*', { cwd: __dirname});
-    
+    // console.log(111, __dirname);
     // create your generators here
-    // plop.setGenerator('controller', {
-    //     description: 'application controller logic',
-    //     prompts: [{
-    //         type: 'input',
-    //         name: 'name',
-    //         message: 'controller name please'
-    //     }],
-    //     actions(obj) {
-    //         const actions = [];
-    //         files.forEach(file => {
-    //             let ph = file.slice(9);
-    //             let p = path.join(process.cwd(), obj.name, ph)
-    //             let tmp = {
-    //                 type: 'add',
-    //                 path: p,
-    //                 templateFile: file
-    //             };
+    plop.setGenerator('controller', {
+        description: 'application controller logic',
+        prompts: [{
+            type: 'input',
+            name: 'name',
+            message: 'controller name please'
+        }],
+        actions(obj) {
+            const actions = [];
+            files.forEach(file => {
+                let ph = file.slice(9);
+                let p = path.join(process.cwd(), obj.name, ph)
+                let tmp = {
+                    type: 'add',
+                    path: p,
+                    templateFile: file
+                };
 
-    //             actions.push(tmp);
-    //         })
-    //         return actions;
-    //     }
-    // });
-    plop.setHelper('upperCase', function (text) {
-        return text.toUpperCase();
+                actions.push(tmp);
+            })
+            // console.log(actions);
+
+            return actions;
+        }
     });
 };
